d7bb66de7c159726aa2aff987d7b14a7005de25f
mishraranjan <113978396+mishraranjan@users.noreply.github.com>
2023-11-01 21:28:19 +0530

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,35 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import errorHandle from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signup = async(req,res,next) => {
- const {username,email,password} = req.body;
- const hashedPassword = bcryptjs.hashSync(password,10);
- const newUser = new User({username,email,password:hashedPassword});
- try{
-    await newUser.save()
-    res.status(201).json('User created succcessfully');
- }catch(error){
-   next(error);
- }
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User created succcessfully");
+  } catch (error) {
+    next(error);
+  }
+};
 
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandle(404, "User not Found"));
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if (!validPassword) return next(errorHandle(401, "Wrong Credentials"));
+      const token = jwt.sign({ _id: validUser._id }, process.env.SECRET_KEY);
+      const {password: pass, ...rest} = validUser._doc;
+      res
+        .cookie("access_token", token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signin, signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
-
+router.post('/signin',signin);
 router.post('/signup',signup);
+
 
 export default router;
